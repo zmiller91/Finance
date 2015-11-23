3bbc2a4009b1bb805a322dc656f88eff490b36dd
unknown <2.4.8.16.32.sixtyfour@gmail.com>
2015-11-22 20:43:36 -0700

    deleting files

--- Parameters/BalanceSheet.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'zmiller'
-
-"""
-Contains all parameters that relate
-to the company's balance sheet
-
-Taken from https://ilmusaham.wordpress.com/tag/stock-yahoo-data/
-"""
-BOOK_VALUE = 'b4'
